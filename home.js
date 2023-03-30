15b3f5b9035e76d9a977c4ed442c2f54da0676a8
Raymond Wong <raymondwong1701@gmail.com>
2023-03-29 17:45:03 -0700

    Setup for button functionality

---
+++ home.js
@@ -0,0 +1,18 @@
+
+function expandAboutMe() {
+    console.log("Hi");
+}
+
+function expandExperience() {
+
+}
+
+// TODO add preview for resume instead of google docs link?
+
+function expandProjects() {
+
+}
+
+function expandHobbies() {
+
+}
