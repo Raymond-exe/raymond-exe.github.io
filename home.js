148eb7e5a1ec409346909fa6bb47656ca0691ab4
Raymond Wong <raymondwong1701@gmail.com>
2023-04-13 11:18:53 -0700

    WIP pop-ups

--- home.js
+++ home.js
@@ -1,18 +1,30 @@
+const TINT_ALPHA = 0.75;
 
-function expandAboutMe() {
-    console.log("Hi");
+async function expandSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (!element.classList.contains("expanded"))
+        element.classList.add("expanded");
+
+    document.getElementById("overlay").style.zIndex = 10;
+    document.getElementById("tint").classList.add("shaded");
+}
+async function hideSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    while (element.classList.contains("expanded"))
+        element.classList.remove("expanded");
+
+    document.getElementById("overlay").style.zIndex = -10;
+    document.getElementById("tint").classList.remove("shaded");
 }
 
-function expandExperience() {
+async function screenTint(darken = true) {
+    const element = document.getElementById("tint");
 
+    document.getElementById("overlay").style.zIndex = darken ? 10 : -10;
+
+    // TODO
 }
 
-// TODO add preview for resume instead of google docs link?
-
-function expandProjects() {
-
-}
-
-function expandHobbies() {
-
-}
+function lerp (a, b, alpha){
+    return a*(1-alpha) + b*alpha;
+  }
