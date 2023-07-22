2545befa42f107466b2141dd07e85c167ac4cda8
Raymond Wong <raymondwong1701@gmail.com>
2023-07-21 23:58:56 -0700

    More progress on category content

--- home.js
+++ home.js
@@ -1,23 +1,36 @@
 const TINT_ALPHA = 0.75;
 
-async function expandSection(sectionId) {
+async function expandSection(sectionId, isSubsection = false) {
     const element = document.getElementById(sectionId);
     if (!element.classList.contains("expanded"))
         element.classList.add("expanded");
 
-    document.getElementById("overlay").style.pointerEvents = 'all';
-    document.getElementById("tint").classList.add("shaded");
+    if (!isSubsection) {
+        document.getElementById("overlay").style.pointerEvents = 'all';
+        document.getElementById("tint").classList.add("shaded");
+    }
 }
 
-async function hideSection(sectionId) {
+async function hideSection(sectionId, isSubsection = false) {
     const element = document.getElementById(sectionId);
     while (element.classList.contains("expanded"))
         element.classList.remove("expanded");
 
-    document.getElementById("overlay").style.pointerEvents = 'none';
-    document.getElementById("tint").classList.remove("shaded");
+    if (!isSubsection) {
+        document.getElementById("overlay").style.pointerEvents = 'none';
+        document.getElementById("tint").classList.remove("shaded");
+    }
 }
 
-function lerp (a, b, alpha){
+async function toggleSection(sectionId, isSubsection = false) {
+    const element = document.getElementById(sectionId);
+    if (element.classList.contains("expanded")) {
+        hideSection(sectionId, isSubsection);
+    } else {
+        expandSection(sectionId, isSubsection);
+    }
+}
+
+function lerp (a, b, alpha) {
     return a*(1-alpha) + b*alpha;
-  }
+}
