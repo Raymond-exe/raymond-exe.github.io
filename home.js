b15a4250f919ccc13d5cfae35993e05d63dc2569
Raymond Wong <raymondwong1701@gmail.com>
2023-08-12 15:47:12 -0700

    Logic to direct URLs to specific sections

--- home.js
+++ home.js
@@ -1,5 +1,21 @@
 const TINT_ALPHA = 0.75;
 
+window.addEventListener('popstate', checkUrl);
+
+async function checkUrl() {
+    const url = window.location.href;
+
+    if (url.includes('#')) {
+        const overlay = document.getElementById('overlay');
+        const id = url.substring(url.lastIndexOf('#') + 1);
+        const element = document.getElementById(id);
+
+        if (element && overlay.contains(element)) {
+            expandSection(id);
+        }
+    }
+}
+
 async function expandSection(sectionId, isSubsection = false) {
     const element = document.getElementById(sectionId);
     if (!element.classList.contains("expanded"))
@@ -19,6 +35,13 @@ async function hideSection(sectionId, isSubsection = false) {
     if (!isSubsection) {
         document.getElementById("overlay").style.pointerEvents = 'none';
         document.getElementById("tint").classList.remove("shaded");
+    }
+
+    // removes the id from the URL
+    const url = window.location.href;
+    if (url.includes('#')) {
+        console.log(url.substring(url.lastIndexOf('/'), url.lastIndexOf('#') ));
+        window.history.pushState("", "", url.substring(url.lastIndexOf('/'), url.lastIndexOf('#')));
     }
 }
 
